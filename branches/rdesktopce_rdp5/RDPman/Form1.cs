96377e5e56cd1c5186bb9317f962aec2c1a5f6dc
hjgode@gmail.com <hjgode@gmail.com@3079fd85-3e52-e0d3-615b-9698352b7ddc>
2014-01-21 10:47:43 +0000

    started an external launcher for rdekstopce

---
+++ branches/rdesktopce_rdp5/RDPman/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RDPman
+{
+    public partial class Form1 : Form
+    {
+        rdp_settings _settings;
+        public Form1()
+        {
+            InitializeComponent();
+            _settings = new rdp_settings();
+        }
+        void initForm()
+        {
+            txtHost.Text = _settings._sHostname;
+            txtPass.Text = _settings._sPass;
+            txtPort.Text = _settings._iPort.ToString();
+            txtUser.Text = _settings._sUser;
+            txtDomain.Text = _settings._sDomain;
+
+            txtWidth.Text = _settings._iWidth.ToString();
+            txtHeight.Text = _settings._iHeight.ToString();
+            setBPP(_settings._iBPP);
+            if(_settings._iFullscreen==1)
+                chkFullscreen.Checked = true;
+            else
+                chkFullscreen.Checked = false;
+        }
+
+        void setBPP(int iBPP)
+        {
+            for (int i = 0; i < cboColorDepth.Items.Count; i++)
+            {
+                if (cboColorDepth.Items[i].ToString() == iBPP.ToString())
+                {
+                    cboColorDepth.SelectedIndex = i;
+                    break;
+                }
+            }
+        }
+        int getBPP()
+        {
+            int iRet = 8;
+            if (cboColorDepth.SelectedIndex == -1)
+                return iRet;
+            int iTest=0;
+            string s = cboColorDepth.SelectedItem.ToString();
+            try
+            {
+                iTest = Convert.ToInt16(s);
+                if (iTest == 8 || iTest == 15 || iTest == 16)
+                    iRet = iTest;
+            }
+            catch (Exception)
+            {
+            }
+            return iRet;
+        }
+    }
+}
---
+++ branches/rdesktopce_rdp5/RDPman/rdp_settings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDPman
+{
+    class rdp_settings
+    {
+        //-g geometry
+        public int _iWidth = 240;
+        public int _iHeight = 320;
+
+        //-t port
+        public int _iPort = 3389;
+
+        //-a bpp BitsPerPixel, 8 or 15 or 16
+        public int _iBPP = 16;
+
+        //-f fullscreen
+        public int _iFullscreen = 0;
+
+        //-u user name
+        public string _sUser = "";
+
+        //-p password
+        public string _sPass = "";
+
+        //-d domain
+        public string _sDomain = "";
+
+        //-s shell
+        public string _sShell = "";
+
+        //-c directory
+        public string _sWorkingDir = "";
+
+        //-n hostname
+        public string _sHostname = "";
+
+        //-x clipboard, currently not supported
+        public int _iClipboard = 0;
+
+        //-b barcode reader support
+        public int _iBarcodeReaderSupport = 0;
+
+    }
+}
